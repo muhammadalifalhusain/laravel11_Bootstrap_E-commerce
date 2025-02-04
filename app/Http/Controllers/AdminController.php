20af75b93e9649c1e9832841dc2465d02c34331c
muhammadalifalhusain <muhalifalhusain@gmail.com>
2025-02-04 19:33:24 +0700

    feat : admin panel & feat : middleware  user or admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function index(){
-        return view('user.index');
+        return view('admin.index');
     }
 }
