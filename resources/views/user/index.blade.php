fdef3bebd1a515bac2e49cd713568111fa9ab8a5
muhammadalifalhusain <muhalifalhusain@gmail.com>
2025-02-05 20:53:01 +0700

    update README.md

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,8 +10,8 @@
         </div>
         <div class="col-lg-9">
           <div class="page-content my-account__dashboard">
-            <p>Hello <strong>{{Auth::user()->name}}</strong></p>
-            <p>From your account dashboard you can view your <a class="unerline-link" href="account_orders.html">recent
+            <p >Hello <strong class="fs-3">{{Auth::user()->name}}</strong></p>
+            <p class="fs-5">From your account dashboard you can view your <a class="unerline-link" href="account_orders.html">recent
                 orders</a>, manage your <a class="unerline-link" href="account_edit_address.html">shipping
                 addresses</a>, and <a class="unerline-link" href="account_edit.html">edit your password and account
                 details.</a></p>
