cfd161b4f848016740a0384127ecc118ae8486d1
muhammadalifalhusain <muhalifalhusain@gmail.com>
2025-02-08 22:12:29 +0700

    feat : add brands menu admin panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,12 +1,18 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers; // Namespace harus di atas
 
+use App\Models\Brand; // Import model setelah namespace
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index(){
         return view('admin.index');
+    }
+
+    public function brands(){
+        $brands = Brand::orderBy('id', 'DESC')->paginate(10); // Perbaikan 'DESC-'
+        return view('admin.brands', compact('brands'));
     }
 }
