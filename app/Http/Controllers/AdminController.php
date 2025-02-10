795b5d7f26594e1ebbb8e1ca911e8dba16f87ed6
muhammadalifalhusain <muhalifalhusain@gmail.com>
2025-02-10 18:56:35 +0700

    add convert slug brand, fix preview img

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers; // Namespace harus di atas
 
 use App\Models\Brand; // Import model setelah namespace
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image; // Tambahkan ini
 
 class AdminController extends Controller
 {
@@ -14,5 +17,48 @@ class AdminController extends Controller
     public function brands(){
         $brands = Brand::orderBy('id', 'DESC')->paginate(10); // Perbaikan 'DESC-'
         return view('admin.brands', compact('brands'));
+    }
+
+    public function add_brand()
+    {
+        return view('admin.brand-add');
+    }
+
+    public function brand_store(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug',
+            'image' => 'mimes:png,jpg,jpeg|max:2048' // Perbaikan max2048 menjadi max:2048
+        ]);
+
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+
+        $image = $request->file('image');
+        $file_extention = $image->extension(); // Perbaikan cara mendapatkan ekstensi file
+        $file_name = Carbon::now()->timestamp . '.' . $file_extention; // Perbaikan timestamp()
+
+        // Simpan gambar ke dalam folder
+        $image->move(public_path('uploads/brands'), $file_name);
+
+        // Panggil fungsi untuk generate thumbnail
+        $this->GenerateBrandThumbnailsImage($image, $file_name);
+
+        $brand->image = $file_name;
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand Berhasil di Tambahkan');
+    }
+
+    public function GenerateBrandThumbnailsImage($image, $imageName)
+    {
+        $destinationPath = public_path('uploads/brands');
+
+        // Perbaikan pemrosesan gambar menggunakan Intervention Image
+        $img = Image::make($image->path())->resize(124, 124, function($constraint){
+            $constraint->aspectRatio();
+        });
+
+        $img->save($destinationPath . '/' . $imageName);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,8 @@ Route::middleware(['auth'])->group(function(){
 Route::middleware(['auth',AuthAdmin::class])->group(function(){
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
     Route::get('/admin/brands', [AdminController::class, 'brands'])->name('admin.brands');
+    Route::get('/admin/brand/add', [AdminController::class, 'add_brand'])->name('admin.brand.add');
+    Route::put('/admin/brand/store', [AdminController::class, 'brand_store'])->name('admin.brand.store');
+
 
 });
